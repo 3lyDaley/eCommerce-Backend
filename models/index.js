e6517429afcdda095c58c89e96e533cfa4c4426f
E Daley <100460009+3lyDaley@users.noreply.github.com>
2022-08-22 17:01:14 -0700

    refactor and demo videos

--- models/index.js
+++ models/index.js
@@ -3,14 +3,37 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { ProvidedRequiredArgumentsOnDirectivesRule } = require('graphql/validation/rules/ProvidedRequiredArgumentsRule');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+
+  foreignKey: 'product_id',
+  onDelete:  'SET NULL'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tags',
+
+  foreignKey: 'tag_id',
+  onDelete: 'SET NULL'
+})
+
 
 module.exports = {
   Product,
